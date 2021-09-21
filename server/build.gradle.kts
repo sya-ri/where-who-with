b11d682c8071ecefd0a426c83835c80a2a38e848
sya-ri <sya79lua@gmail.com>
2021-09-22 02:03:16 +0900

    chore(server): 依存関係のバージョンを更新

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -4,7 +4,7 @@ import groovy.lang.Closure
 
 plugins {
     kotlin("jvm") version "1.5.31"
-    id("org.jmailen.kotlinter") version "3.5.0"
+    id("org.jmailen.kotlinter") version "3.6.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("com.github.ben-manes.versions") version "0.39.0"
     id("com.palantir.git-version") version "0.12.3"
@@ -33,7 +33,7 @@ dependencies {
     shadowImplementation("org.jetbrains.exposed:exposed-dao:0.34.2")
     shadowImplementation("org.jetbrains.exposed:exposed-jdbc:0.34.2")
     shadowImplementation("org.jetbrains.exposed:exposed-java-time:0.34.2")
-    shadowRuntimeOnly("org.xerial:sqlite-jdbc:3.36.0.2")
+    shadowRuntimeOnly("org.xerial:sqlite-jdbc:3.36.0.3")
 }
 
 application {
