49cb0ed850c53b4352f8add789de307bd7bf8f08
sya-ri <sya79lua@gmail.com>
2021-09-25 20:46:27 +0900

    feat(server): ExecutionPlatform を使わなくてもコマンドを登録できるようにした

--- server/src/main/kotlin/dev/s7a/w3/server/cli/command/action/desk/list.kt
+++ server/src/main/kotlin/dev/s7a/w3/server/cli/command/action/desk/list.kt
@@ -1,13 +1,12 @@
 package dev.s7a.w3.server.cli.command.action.desk
 
-import dev.s7a.w3.server.cli.api.ExecutionPlatform
 import dev.s7a.w3.server.cli.util.useDatabaseOnce
 import dev.s7a.w3.server.database.entity.Desk
 
 /**
  * 受付の一覧を表示する
  */
-fun ExecutionPlatform.deskList() {
+fun deskList() {
     val desks = useDatabaseOnce {
         Desk.all().toList()
     }
--- server/src/main/kotlin/dev/s7a/w3/server/cli/command/action/version.kt
+++ server/src/main/kotlin/dev/s7a/w3/server/cli/command/action/version.kt
@@ -1,11 +1,9 @@
 package dev.s7a.w3.server.cli.command.action
 
-import dev.s7a.w3.server.cli.api.ExecutionPlatform
-
 /**
  * バージョンを表示する
  */
-fun ExecutionPlatform.version() {
+fun version() {
     val resource = ClassLoader.getSystemResourceAsStream("version.txt")
     val version = resource?.bufferedReader()?.readText() ?: "null"
     println("Version: $version")
