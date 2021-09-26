eb027aca0f399a8561468bef2e124ec610a130f9
sya-ri <sya79lua@gmail.com>
2021-09-26 19:17:42 +0900

    perf(server): 既にデータベースの初期化が行われていれば再接続を行わないようにした

--- server/src/main/kotlin/dev/s7a/w3/server/database/database.kt
+++ server/src/main/kotlin/dev/s7a/w3/server/database/database.kt
@@ -3,12 +3,14 @@ package dev.s7a.w3.server.database
 import dev.s7a.w3.server.database.table.tables
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
  * データベースの初期化を行う
  */
 fun setupDatabase() {
+    if (TransactionManager.isInitialized()) return
     Database.connect("jdbc:sqlite:database.sqlite", "org.sqlite.JDBC")
     transaction {
         create(*tables.toTypedArray())
