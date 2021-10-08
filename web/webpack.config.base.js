7859f76eba08433473156b97bff7e349a69dbf37
sya-ri <sya79lua@gmail.com>
2021-10-08 16:55:43 +0900

    chore(web): tailwindcss を導入

--- web/webpack.config.base.js
+++ web/webpack.config.base.js
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const TailwindCss = require('tailwindcss');
+const Autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -19,11 +23,39 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
       },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [TailwindCss, Autoprefixer],
+              },
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './html/index.html',
+    }),
+    /* FIXME static が追加されたときにコメントアウトを外す
     new CopyWebpackPlugin({
       patterns: [{ from: 'static' }],
+    }),
+    */
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
     }),
   ],
   optimization: {
