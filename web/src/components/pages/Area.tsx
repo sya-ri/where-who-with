b5b4958c7b7c6afd329ebf86b1f618282b7fb033
sya-ri <sya79lua@gmail.com>
2021-10-11 18:35:24 +0900

    feat(web): /area ページを作成

--- web/src/components/pages/Area.tsx
+++ web/src/components/pages/Area.tsx
@@ -1,5 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import QrReader from 'react-qr-reader';
+import { Button, Typography } from '@mui/material';
+import * as api from '../../api/method';
 
 type Params = {
   uuid: string;
@@ -7,7 +10,66 @@ type Params = {
 
 const Area: FC = () => {
   const { uuid } = useParams<Params>();
-  return <>Area / {uuid}</>;
+  const [userUuid, setUserUuid] = useState<string | null>();
+  return (
+    <div>
+      <div>
+        <QrReader
+          onScan={(data) => {
+            if (data) {
+              setUserUuid(data.substring(data.lastIndexOf('/') + 1));
+            }
+          }}
+          onError={(error) => {
+            console.error(error);
+          }}
+        />
+      </div>
+      <div className="text-center">
+        <Typography>{userUuid || 'QRコードをスキャンしてください'}</Typography>
+        <div>
+          <Button
+            variant="contained"
+            disabled={!userUuid}
+            onClick={() => {
+              if (userUuid) {
+                api
+                  .postLogJoin({ area_uuid: uuid, user_uuid: userUuid })
+                  .then(() => {
+                    setUserUuid(null);
+                  })
+                  .catch((error) => {
+                    console.error(error);
+                    setUserUuid(null);
+                  });
+              }
+            }}
+          >
+            入室
+          </Button>
+          <Button
+            variant="contained"
+            disabled={!userUuid}
+            onClick={() => {
+              if (userUuid) {
+                api
+                  .postLogLeave({ area_uuid: uuid, user_uuid: userUuid })
+                  .then(() => {
+                    setUserUuid(null);
+                  })
+                  .catch((error) => {
+                    console.error(error);
+                    setUserUuid(null);
+                  });
+              }
+            }}
+          >
+            退室
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Area;
