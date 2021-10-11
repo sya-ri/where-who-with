a20c349d4449148e270c1a85c8d231022c9310d8
sya-ri <sya79lua@gmail.com>
2021-10-12 01:20:03 +0900

    feat(web): NotFound ページを追加

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -5,6 +5,7 @@ import Desk from './pages/Desk';
 import * as Pages from '../Pages';
 import DeskView from './pages/DeskView';
 import User from './pages/User';
+import NotFound from './pages/NotFound';
 
 const App: FC = () => (
   <div className="app h-full">
@@ -15,6 +16,7 @@ const App: FC = () => (
           <Route exact path={Pages.Desk(':uuid')} component={Desk} />
           <Route exact path={Pages.DeskView(':uuid')} component={DeskView} />
           <Route exact path={Pages.User(':uuid')} component={User} />
+          <Route exact component={NotFound} />
         </Switch>
       </BrowserRouter>
     </div>
