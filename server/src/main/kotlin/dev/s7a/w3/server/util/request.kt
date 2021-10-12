f53f84763d92acd1498aab8da7cde4f5f45bb57f
sya-ri <sya79lua@gmail.com>
2021-10-12 14:47:38 +0900

    fix: 不正なJsonに対して500エラーが出る問題を修正

--- server/src/main/kotlin/dev/s7a/w3/server/util/request.kt
+++ server/src/main/kotlin/dev/s7a/w3/server/util/request.kt
@@ -2,17 +2,17 @@ package dev.s7a.w3.server.util
 
 import io.ktor.application.ApplicationCall
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.ContentTransformationException
 import io.ktor.request.receive
 import io.ktor.response.respond
+import kotlinx.serialization.SerializationException
 
 /**
- * [ApplicationCall.receive] を実行し、取得できなければ [HttpStatusCode.BadRequest] を返す
+ * [ApplicationCall.receive] を実行し、取得できなければ [HttpStatusCode.BadRequest], [HttpStatusCode.UnsupportedMediaType] を返す
  * @return 取得できればその値。できなければ null
  */
 suspend inline fun <reified T : Any> ApplicationCall.receiveOrRespondBadRequest(): T? = try {
     receive()
-} catch (ex: ContentTransformationException) {
+} catch (ex: SerializationException) {
     respond(HttpStatusCode.BadRequest)
     null
 }
