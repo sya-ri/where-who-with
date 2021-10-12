0eba80a1a76fd83f41c054a47214b1f63fa2affc
sya-ri <sya79lua@gmail.com>
2021-10-12 15:22:34 +0900

    fix(server): 不正なUUIDに対して500エラーが出る問題を修正

--- server/src/main/kotlin/dev/s7a/w3/server/util/UUIDSerializer.kt
+++ server/src/main/kotlin/dev/s7a/w3/server/util/UUIDSerializer.kt
@@ -1,6 +1,7 @@
 package dev.s7a.w3.server.util
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
@@ -14,7 +15,11 @@ object UUIDSerializer : KSerializer<UUID> {
     override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): UUID {
-        return UUID.fromString(decoder.decodeString())
+        return try {
+            UUID.fromString(decoder.decodeString())
+        } catch (ex: IllegalArgumentException) {
+            throw SerializationException()
+        }
     }
 
     override fun serialize(encoder: Encoder, value: UUID) {
