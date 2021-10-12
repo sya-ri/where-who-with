4698dccad78572e9230c202cfda8d642b6265309
sya-ri <sya79lua@gmail.com>
2021-10-12 15:44:49 +0900

    feat(server): アクセスログを表示するようにした

--- server/src/main/kotlin/dev/s7a/w3/server/server.kt
+++ server/src/main/kotlin/dev/s7a/w3/server/server.kt
@@ -4,13 +4,18 @@ import dev.s7a.w3.server.route.route
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CORS
+import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.toLogString
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.routing.Routing
 import io.ktor.routing.routing
 import io.ktor.serialization.json
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
+import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
 
 /**
  * サーバーを起動する
@@ -29,6 +34,17 @@ fun Application.application() {
     install(CORS) {
         anyHost()
         header(HttpHeaders.ContentType)
+    }
+    install(CallLogging) {
+        level = Level.INFO
+        logger = LoggerFactory.getLogger("Log")
+        format { call ->
+            val requestLog = call.request.toLogString()
+            when (val status = call.response.status() ?: "Unhandled") {
+                HttpStatusCode.Found -> "$status: $requestLog -> ${call.response.headers[HttpHeaders.Location]}"
+                else -> "$status: $requestLog"
+            }
+        }
     }
     routing(Routing::route)
 }
